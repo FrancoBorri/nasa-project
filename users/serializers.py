e656f27396c786b6cd5ad9e3f75a8bcac18fd7b7
FrancoBorri <francoborri11@gmial.com>
2025-02-27 16:37:10 -0300

    problema con importaciones

--- users/serializers.py
+++ users/serializers.py
@@ -6,3 +6,26 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+    def validate_username(self, username):
+        if User.objects.filter(user_name=username).exists():
+            raise serializers.ValidationError("Username already exists")
+        return username
+
+    def validate_email(self, email):
+        # realizar consultas en la base de datos filter(), get(), create(), update(), etc.
+        if User.objects.filter(email=email).exists():
+            # raise lanzador de errores en python
+            raise serializers.ValidationError("Email already exists")
+        return email
+
+    def validate_password(self, password):
+        if len(password) < 8:
+            raise serializers.ValidationError(
+                "La contraseña debe contener al menos 8 caracteres"
+            )
+        if not any(char.isalpha() for char in password):
+            raise serializers.ValidationError(
+                "La contraseña debe contener al menos una letra"
+            )
+        return password
--- users/views.py
+++ users/views.py
@@ -1,6 +1,15 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
-from users.serializers import UserSerializer
-from django.contrib.auth.models import User
+from .models import User
+from .serializers import UserSerializer
+
+
+
+class User_create(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.validate_email(request.data['email']):
+            serializer.save() # Guarda el objeto en la base de datos
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
